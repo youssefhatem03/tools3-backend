f7cc1d273357c4298c5c1a0fc4f0037f1abcc198
Youssef Hatem <115034716+youssefhatem03@users.noreply.github.com>
2024-12-24 14:09:39 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const Pool = require('pg').Pool;
 const bcrypt = require('bcryptjs');
 const pool = new Pool({
-  user: 'postgres',
-  host: 'db',
-  database: 'API',
-  password: '12345678',
-  port: 5432,
+  user: 'postgres', // Matches the Role name
+  host: 'postgresql', // Matches the Service name
+  database: 'postgres', // Matches the Database name
+  password: '12345678', // Matches the password
+  port: 5432, // Default PostgreSQL port
 });
+
 module.exports = pool;
